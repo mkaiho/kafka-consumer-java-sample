008c6fb473438770892a4d2aa6cabe3503b5bb18
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2020-03-22 23:11:41 +0900

    Add files generated by Spring Initializr

---
+++ src/test/java/com/mkaiho/sample/kakafkaconsumer/KakafkaSubscriberJavaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkaiho.sample.kakafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakafkaConsumerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
