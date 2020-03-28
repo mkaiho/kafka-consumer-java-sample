8d37604751e3e508abd1e62f56ba897c6e86c618
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2020-03-29 02:00:50 +0900

    Add Messaging Service

---
+++ src/main/java/com/mkaiho/sample/kakafkaconsumer/application/service/MessagingService.java
@@ -0,0 +1,9 @@
+package com.mkaiho.sample.kakafkaconsumer.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessagingService {
+    public void consume() {
+    }
+}
---
+++ src/test/groovy/com/mkaiho/sample/kakafkaconsumer/application/service/MessagingServiceSpec.groovy
@@ -0,0 +1,7 @@
+package com.mkaiho.sample.kakafkaconsumer.application.service
+
+import spock.lang.Specification
+
+class MessagingServiceSpec extends Specification {
+    def "Consuming messages normally"() {}
+}
