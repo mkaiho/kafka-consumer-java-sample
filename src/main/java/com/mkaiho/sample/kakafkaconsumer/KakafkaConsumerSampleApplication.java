fbe0b6c43e08a7f6aa59683026e02e80e4ebffc9
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2020-03-29 01:52:26 +0900

    introduce spock testing framework

--- src/main/java/com/mkaiho/sample/kakafkaconsumer/KakafkaConsumerSampleApplication.java
+++ src/main/java/com/mkaiho/sample/kakafkaconsumer/KakafkaConsumerSampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KakafkaConsumerSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KakafkaConsumerSampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KakafkaConsumerSampleApplication.class, args);
+    }
 
 }
---
+++ src/test/groovy/com/mkaiho/sample/kakafkaconsumer/KakafkaConsumerSampleApplicationSpecs.groovy
@@ -0,0 +1,12 @@
+package com.mkaiho.sample.kakafkaconsumer
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@SpringBootTest
+class KakafkaConsumerSampleApplicationSpecs extends Specification {
+    @Unroll
+    def contextLoads() {
+    }
+}
